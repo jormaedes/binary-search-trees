5c8a73f007334933c34176541dbfe026d6de3201
Jormaedes Luis <jormahed@gmail.com>
2026-03-11 16:39:53 +0100

    feat(Tree.js): adicionar metodo de encher a arvore

--- Tree.js
+++ Tree.js
@@ -8,4 +8,19 @@ export class Tree {
 	_filter(arr) {
 		return [...new Set(arr.sort((a, b) => a - b))];
 	}
+
+	_fillTree(arr, start, end) {
+		if (start > end) return null;
+		const mid = start + Math.floor((end - start) / 2);
+		let root = new Node(arr[mid]);
+
+		root.left = this._fillTree(arr, start, mid - 1);
+		root.right = this._fillTree(arr, mid + 1, end);
+		return (root);
+	}
+
+	_buildTree(arr) {
+		arr = this._filter(arr);
+		return this._fillTree(arr, 0, arr.length - 1);
+	}
 }
